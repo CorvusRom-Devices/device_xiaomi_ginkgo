2a7168209cf76fcfa18754fed1b62c3e591197f5
Introdructor <56420114+Introdructor@users.noreply.github.com>
2021-02-06 14:20:43 +0530

    ginkgo: Track sdclang from PA

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,3 +27,4 @@ git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b  arrow-1
 git clone https://github.com/ArrowOS/android_hardware_qcom_media.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/media
 git clone https://github.com/ArrowOS/android_vendor_qcom_opensource_power.git vendor/qcom/opensource/power
 git clone https://github.com/ArrowOS/android_vendor_qcom_opensource_interfaces.git vendor/qcom/opensource/interfaces
+git clone https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang.git prebuilts/clang/host/linux-x86/clang-sdllvm
